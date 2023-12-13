d7ddfc37e300b27dee3529962f1e4e452d201707
EmilioArreola <emilioabrham153@gmail.com>
2023-12-12 22:33:40 -0600

    web nice

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\CitaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\DireccionClienteController;
+use App\Http\Controllers\DireccionController;
+use App\Http\Controllers\DireccionUsuarioController;
+use App\Http\Controllers\HistorialClinicoController;
+use App\Http\Controllers\MascotaController;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +25,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Creamos un grupo de rutas protegidas para los controladores
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RolController::class);
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('citas', CitaController::class);
+    Route::resource('clientes', ClienteController::class);
+    Route::resource('direccionClientes', DireccionClienteController::class);
+    Route::resource('direcciones', DireccionController::class);
+    Route::resource('direccionUsuarios', DireccionUsuarioController::class);
+    Route::resource('grupos', HistorialClinicoController::class);
+    Route::resource('mascotas', MascotaController::class);
+    Route::resource('productos', ProductoController::class);
 });
